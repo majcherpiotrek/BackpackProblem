bf093dffd0f295d818f0a431d21a4f9222fb00f4
piotrek <piotr.majcher94@gmail.com>
2017-01-04 13:33:24 +0100

    Dodano statyczną metodę do pomiaru czasu wykonania algorytmu programowania dynamicznego ( w klasie AlgorithmPeformanceTests) + zmiana na jedną metodę przeciążoną dla każdego algorytmu.

--- backpack/AlgorithmPeformanceTests.java
+++ backpack/AlgorithmPeformanceTests.java
@@ -11,7 +11,7 @@ import static java.lang.System.nanoTime;
  * Created by piotrek on 09.12.16.
  */
 public class AlgorithmPeformanceTests {
-    public static double elapsedTimeMilisec_BB(BranchAndBoundAlgorithm branchAndBound){
+    public static double elapsedTimeMilisec(BranchAndBoundAlgorithm branchAndBound){
         long start;
         long elapsedTimeNano;
 
@@ -22,7 +22,7 @@ public class AlgorithmPeformanceTests {
         return (double)elapsedTimeNano/(double)1000000;
     }
 
-    public static double elapsedTimeMilisec_BF(BruteForceAlgorithm bruteForce){
+    public static double elapsedTimeMilisec(BruteForceAlgorithm bruteForce){
         long start;
         long elapsedTimeNano;
 
@@ -33,7 +33,7 @@ public class AlgorithmPeformanceTests {
         return (double)elapsedTimeNano/(double)1000000;
     }
 
-    public static double elapsedTimeMilisec_DP(DynamicProgrammingAlgorithm dynamicProg){
+    public static double elapsedTimeMilisec(DynamicProgrammingAlgorithm dynamicProg){
         long start;
         long elapsedTimeNano;
 
